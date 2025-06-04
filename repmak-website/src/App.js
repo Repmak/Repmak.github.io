4fd535485888f825ccf6c7a9a335527a932e5d85
Justin Kamper <justinjkamper@gmail.com>
2025-06-04 23:44:58 +0100

    update name and tagline

--- repmak-website/src/App.js
+++ repmak-website/src/App.js
@@ -4,11 +4,12 @@
 // git push origin main
 
 import './homepage.css';
-import './project-page.css';
+import './projects.css';
 import Connect4 from "./assets/connect 4.gif"
 import FinancialRiskAnalysis from "./assets/financial risk analysis.png"
 import AuditoriumBookingSystem from "./assets/auditorium booking system.gif"
 import { useEffect, useRef } from "react";
+import React from "react";
 import { motion, AnimatePresence } from 'motion/react';
 
 
@@ -17,10 +18,17 @@ const sections = [
         id: 1,
         content: (
             <div className="homepage-container">
-                <div className="homepage-name">
-                    {"JUSTIN KAMPER".split("").map((char, i) => (
-                        <span key={i}>{char === " " ? "\u00A0" : char}</span>
-                    ))}
+                <div className="homepage-name dotted-text">
+                    {"JUSTIN KAMPER".split("").map((char, i) =>
+                        char === " " ? (
+                            <React.Fragment key={i}>
+                                {" "}
+                                <wbr/>
+                            </React.Fragment>
+                        ) : (
+                            <span key={i}>{char}</span>
+                        )
+                    )}
                 </div>
                 <div className="homepage-tagline">
                     I'm a penultimate-year Computer Science student at the University of St Andrews who enjoys solving meaningful problems through forward-thinking software design.
@@ -29,33 +37,40 @@ const sections = [
                 </div>
                 <div className="homepage-links">
                     <a href="https://github.com/Repmak" target="_blank">GitHub</a>
-                    <a href="https://www.linkedin.com/in/justin-kamper/" target="_blank">Linkedin</a>
+                    <a href="https://www.linkedin.com/in/justin-kamper/" target="_blank">LinkedIn</a>
                 </div>
             </div>
         )
-    },
+     },
     {
         id: 2,
         content: (
             <div className="project-container">
-                <div className="project-header">
-                    <div className="project-name">
-                        {"Financial Risk Forecasting".split("").map((char, i) => (
-                            <span key={i}>{char === " " ? "\u00A0" : char}</span>
-                        ))}
-                    </div>
-                    <div className="project-development-dates">
-                        In development since 06/2025
-                    </div>
+                <div className="project-name dotted-text">
+                    {"Financial Risk Forecasting".split("").map((char, i) =>
+                        char === " " ? (
+                            <React.Fragment key={i}>
+                                {" "}
+                                <wbr/>
+                            </React.Fragment>
+                        ) : (
+                            <span key={i}>{char}</span>
+                        )
+                    )}
+                </div>
+
+                <div className="project-dates">
+                    In development since 06/2025
                 </div>
 
                 <div className="project-content">
-                    <figure className="project-image">
+                    <figure>
                         <img src={FinancialRiskAnalysis} alt="Financial Risk Analysis demo"/>
                         <figcaption>
                             Scheduling tasks and generating evaluations of associated financial risks.
                         </figcaption>
                     </figure>
+
                     <div className="project-tagline">
                         A TensorFlow-based tool designed to optimise task scheduling by analysing data from previous projects. The backend uses Flask for efficient routing, while the frontend is built with React to provide a responsive user interface.
                     </div>
@@ -67,15 +82,21 @@ const sections = [
         id: 3,
         content: (
             <div className="project-container">
-                <div className="project-header">
-                    <div className="project-name">
-                        {"Connect 4".split("").map((char, i) => (
-                            <span key={i}>{char === " " ? "\u00A0" : char}</span>
-                        ))}
-                    </div>
-                    <div className="project-development-dates">
-                        Developed from 07/2024 - 08/2024
-                    </div>
+                <div className="project-name dotted-text">
+                    {"Connect 4".split("").map((char, i) =>
+                        char === " " ? (
+                            <React.Fragment key={i}>
+                                {" "}
+                                <wbr/>
+                            </React.Fragment>
+                        ) : (
+                            <span key={i}>{char}</span>
+                        )
+                    )}
+                </div>
+
+                <div className="project-dates">
+                    Developed from 07/2024 - 08/2024
                 </div>
 
                 <div className="project-content">
@@ -84,7 +105,8 @@ const sections = [
                         <br/><br/>
                         <b>Fun fact:</b> This was the very code that helped gain permission for second-year direct entry at university.
                     </div>
-                    <figure className="project-image">
+
+                    <figure>
                         <img src={Connect4} alt="Connect 4 demo"/>
                         <figcaption>
                             A quick game of Connect 4.
@@ -98,24 +120,31 @@ const sections = [
         id: 4,
         content: (
             <div className="project-container">
-                <div className="project-header">
-                    <div className="project-name">
-                        {"Auditorium Booking System".split("").map((char, i) => (
-                            <span key={i}>{char === " " ? "\u00A0" : char}</span>
-                        ))}
-                    </div>
-                    <div className="project-development-dates">
-                        Developed from 03/2023 - 05/2023
-                    </div>
+                <div className="project-name dotted-text">
+                    {"Auditorium Booking System".split("").map((char, i) =>
+                        char === " " ? (
+                            <React.Fragment key={i}>
+                                {" "}
+                                <wbr/>
+                            </React.Fragment>
+                        ) : (
+                            <span key={i}>{char}</span>
+                        )
+                    )}
+                </div>
+
+                <div className="project-dates">
+                    Developed from 03/2023 - 05/2023
                 </div>
 
                 <div className="project-content">
-                    <figure className="project-image">
+                    <figure>
                         <img src={AuditoriumBookingSystem} alt="Auditorium Booking System demo"/>
                         <figcaption>
                             Reserving 5 seats for a showing of Ratatouille at 10 PM.
                         </figcaption>
                     </figure>
+
                     <div className="project-tagline">
                         An auditorium booking system built with Flask, HTML, JavaScript, CSS, and an SQL database featuring an intuitive interface for reserving seats to movies and plays.
                     </div>
@@ -143,7 +172,7 @@ export default function App() {
             container.scrollTo({top: next, behavior: "smooth"});
 
             isThrottled = true;
-            setTimeout(() => (isThrottled = false), 1000); // control scroll speed
+            setTimeout(() => (isThrottled = false), 1000);
         };
 
         container.addEventListener("wheel", handleScroll, {passive: false});
@@ -159,13 +188,13 @@ export default function App() {
                 <motion.div
                     key={section.id}
                     className="section"
-                    initial={{ opacity: 0, y: 50 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1.5 }}
-        >
-          {section.content}
-        </motion.div>
-      ))}
-    </div>
-  );
+                    initial={{opacity: 0, y: 0}}
+                    whileInView={{opacity: 1, y: 0}}
+                    transition={{duration: 1.5}}
+                >
+                    {section.content}
+                </motion.div>
+            ))}
+        </div>
+    );
 }
