19ef1e41cd402f008e7733a4c00783a4b0af21fc
Justin Kamper <jkamper@fulcrumgt.com>
2026-01-03 23:38:44 +0000

    about me content

---
+++ repmak-website/src/AboutMe/AboutMe.js
@@ -0,0 +1,12 @@
+import '../App.css';
+import './AboutMe.css';
+import React from "react";
+
+
+export default function AboutMe() {
+    return (
+        <div className="container about-me">
+            JUSTIN KAMPER
+        </div>
+    );
+}
