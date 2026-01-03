74be7c7c70e2c0b70262821e3b3545f491fba33c
Justin Kamper <jkamper@fulcrumgt.com>
2026-01-03 23:38:23 +0000

    projects content

---
+++ repmak-website/src/Projects/Projects.js
@@ -0,0 +1,37 @@
+import '../App.css';
+import './Projects.css';
+import {projects} from "./projects-data";
+
+
+export default function Projects() {
+    return (
+        <>
+            <div className="container project">
+                {projects.map((project) => (
+                    <div className="project">
+                        <h2>
+                            <a href={project.githubUrl} target="_blank" rel="noreferrer">
+                                {project.title}
+                            </a>
+                        </h2>
+
+                        <p className="project-dates">{project.dates}</p>
+
+                        <div className="project-content">
+                            <figure>
+                                <img src={project.image} className="project-image" alt={project.title} />
+                                <figcaption>{project.imageCaption}</figcaption>
+
+                            </figure>
+
+                            <div className="project-tagline">
+                                {project.description}
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </>
+    );
+}
+
---
+++ repmak-website/src/Projects/projects-data.js
@@ -0,0 +1,63 @@
+// import PromptableTraceback from "./assets/promptabletraceback.gif";
+import JustInTime from "../assets/justintime.gif";
+import Jolt from "../assets/jolt.gif";
+import Connect4 from "../assets/connect 4.gif";
+import AuditoriumBookingSystem from "../assets/auditorium booking system.gif";
+
+
+export const projects = [
+    // {
+    //     title: "Atomic Limit Order Book",
+    //     dates: "",
+    //     image: "",
+    //     imageCaption: "",
+    //     description: "",
+    //     githubUrl: "https://github.com/Repmak/atomic-limit-order-book",
+    //     public: true
+    // },
+    {
+        title: "PromptableTraceback",
+        dates: "Ongoing development since 01/2026",
+        image: null,
+        imageCaption: "",
+        description: "",
+        githubUrl: "https://github.com/Repmak/promptable-traceback",
+        public: true
+    },
+    {
+        title: "JustInTime",
+        dates: "06/2025 - 12/2025",
+        image: JustInTime,
+        imageCaption: "Analysing how scheduling tasks impact associated financial risks.",
+        description: "JustInTime is an intelligent project planning tool built with TensorFlow and scikit-learn to forecast delays and risks based on historical project data. By processing inputs such as team assignments, supplier details, budgets, and target completion dates, it identifies potential bottlenecks early and provides actionable insights to optimise scheduling and keep projects on track.",
+        githubUrl: "https://github.com/Repmak/JustInTime",
+        public: false
+    },
+    {
+        title: "Jolt",
+        dates: "Developed from 06/2025 - 06/2025",
+        image: Jolt,
+        imageCaption: "Generating some Indeed job insights when searching for a Sales Assistant position.",
+        description: "A Gemini-powered Chrome extension that gives users some quick insights by matching their job preferences, availability, and experience to ideal roles.",
+        githubUrl: "https://github.com/Repmak/Jolt",
+        public: false
+    },
+    {
+        title: "Connect 4",
+        dates: "Developed from 07/2024 - 08/2024",
+        image: Connect4,
+        imageCaption: "A quick game of Connect 4.",
+        description: "A Connect 4 game built with Pyglet, featuring an interactive GUI and an AI opponent powered by the minimax algorithm. (This project played a key role in securing direct entry into the second year of the Computer Science program at the University of St Andrews.)",
+        githubUrl: "https://github.com/Repmak/Connect-4",
+        public: true
+    },
+    {
+        title: "Auditorium Booking System",
+        dates: "Developed from 03/2023 - 05/2023",
+        image: AuditoriumBookingSystem,
+        imageCaption: "Reserving 5 seats for a showing of Ratatouille at 10 PM.",
+        description: "An auditorium booking system built with Flask, HTML, JavaScript, CSS, and an SQL database featuring an intuitive interface for reserving seats to movies and plays.",
+        githubUrl: "https://github.com/Repmak/Auditorium-Booking-System",
+        public: true
+    }
+];
