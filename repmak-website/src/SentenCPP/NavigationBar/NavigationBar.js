a2c28bc77cf5933b6a2f546f049a174dfeaabb27
Justin Kamper <jkamper@fulcrumgt.com>
2026-01-18 20:03:38 +0000

    nav bar done

--- repmak-website/src/SentenCPP/NavigationBar/NavigationBar.js
+++ repmak-website/src/SentenCPP/NavigationBar/NavigationBar.js
@@ -1,12 +1,40 @@
 import React, {useEffect, useState} from 'react';
-import './SentenCPP.css';
+import { Routes, Route, NavLink } from 'react-router-dom';
+import './NavigationBar.css';
 
 
 export default function NavigationBar() {
     return (
-        <div className="navi">
+        <div className="docs-navigation-bar">
+            <NavLink
+                to="/sentencpp-docs/overview"
+                className="navigation-bar-link"
+            >
+                Overview
+            </NavLink>
 
-            frfr
+            <NavLink
+                to="/sentencpp-docs/quick-start"
+                className="navigation-bar-link"
+            >
+                Quick Start
+            </NavLink>
+
+            <NavLink
+                to="/sentencpp-docs/api-reference"
+                className="navigation-bar-link"
+            >
+                API Reference
+            </NavLink>
+
+            <a
+                href="https://github.com/Repmak/sentenCPP/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="navigation-bar-link"
+            >
+                GitHub
+            </a>
         </div>
     );
 }
